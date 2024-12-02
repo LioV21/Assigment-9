7060b185f9a658fd7a604d4e09c4093cd1e879d2
LioV21 <villanuevalionel52104@gmail.com>
2024-12-02 00:57:58 -0800

    questions answered

---
+++ script.js
@@ -0,0 +1,27 @@
+// 1. Write a console.log() statement that answers the following question: What worked
+// well for you over the last 9 weeks that aided in your learning process?
+
+console.log(
+  "What worked well for you over the last 9 weeks that aided in your learning process?"
+);
+console.log("Practicing coding problems and having clear explanations during lectures and breaking down the problems.");
+
+// 2. Write a console.log() statement that answers the following question: What would you
+// change about the course?
+
+console.log("What would you change about the course?");
+console.log("I would suggest a group project or activity so people can share their ideas.");
+
+// 3. Write a console.log() statement that answers the following question: Did you learn
+// what you expected to learn in this course?
+
+console.log("Did you learn what you expected to learn in this course?");
+console.log("Learned  a lot of JavaScript which also helped by reading the book that was provided. ");
+
+// 4. Write a console.log() statement that answers the following question: What concepts
+// were new to you and what concepts had you encountered already in the past?
+
+console.log(
+  "What concepts were new to you and what concepts had you encountered already in the past?"
+);
+console.log("The new concepts I learned is how you can use JavaScript to interact with HTML and CSS.  ");
